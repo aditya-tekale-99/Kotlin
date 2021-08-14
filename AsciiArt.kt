b41b4b7abce74388bdfed34fbb3a29772f5d00de
Aditya Tekale <84354252+aditya-tekale-99@users.noreply.github.com>
2021-08-14 14:26:24 -0400

    Added working algorithm

--- AsciiArt.kt
+++ AsciiArt.kt
@@ -1,1 +1,32 @@
+// Project: ASCII Art
+// Author: Aditya Tekale
 
+/* Use a multiline comment to draft the image
+     .....
+  .         .
+ .   0  0    .
+  ...........
+   .. __ ..
+  . .|C_|. .
+ .  ......  .
+     .  .
+     .  .
+*/
+
+fun main() {
+    // Use print statements to output image
+    println("     .....")
+    println("  .         .")
+    println(" .   0  0    .")
+    println("  ...........")
+    println("   .. __ ..")
+    println("  . .|C_|. .")
+    println(" .  ......  .")
+    println("     .  .")
+    println("     .  .")
+
+    println("-------------------")
+
+    // Use a single print statement to output image
+    println("     .....\n  .         .\n .   0  0    .\n  ...........\n   .. __ ..\n  . .|_C|. .\n .  ......  .\n     .  .\n     .  .  ")
+}
