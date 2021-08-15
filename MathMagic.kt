f1192f8331d03c44e36f5a388200140fd8f9ff4a
Aditya Tekale <84354252+aditya-tekale-99@users.noreply.github.com>
2021-08-15 13:13:03 -0400

    Create MathMagic.kt

---
+++ MathMagic.kt
@@ -0,0 +1,19 @@
+fun main() { 
+    
+    // This program determines the user's age using  an arithmetic magic trick. 
+    
+    val userName = "Adi"
+    var magicNum: Int
+
+    magicNum = 2 * 5  // Multiplied age first number with 5
+
+    magicNum += 3 
+
+    magicNum *= 2 
+
+    magicNum += 2  //Added second number of age
+
+    magicNum -= 6
+    
+    println("$userName's age is $magicNum") 
+}
